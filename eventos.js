eddc8781848cbdfc1e64d59df7149a532c8cacff
Mateusr002 <100283357+Mateusr002@users.noreply.github.com>
2022-04-25 09:26:21 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,25 @@
+function clickMe(){
+
+    prompt("Clicado");
+}
+
+function focusMe(){
+    
+    console.log ("Fui focado");
+}
+
+function blurMe(){
+
+    console.log ("Fui desfocado");
+}
+
+function changeMe(){
+
+    console.log ("fui alterado");
+}
+
+function overMe(){
+
+    console.log ("Mouse aqui");
+}
+
